5a4d48a384bd4e54ce4af95093ea736d63da5547
Stephano Lioe <stephano.lioe@iodigital.com>
2023-10-26 15:22:50 +0200

    add layout vuetify

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,15 @@
 <template>
-    <div>
+  <VContainer>
+    <VRow>
+      <VCol>
         <h1>Index page</h1>
-          <p>
-              This is the index page. It uses the default layout, which means it will
-              render inside the <code>&lt;slot&gt;</code> of the default layout.
-          </p>
-          <VTextField label="Text field" />
-    </div>
+        <VTextField label="Text field" />
+      </VCol>
+    </VRow>
+  </VContainer>
 </template>
 
-<script>
+<script setup>
 definePageMeta({
   layout: "default",
 });
